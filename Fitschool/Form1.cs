2ef8cab300a05f50790e5f0ff075cf3e845eb1c6
MaxDelissen <maxdelissen17@gmail.com>
2023-11-07 14:55:18 +0100

    Deel voor ophalen gegevens uit MySQL database
    Invoer van een ID mogelijk, wanneer gevonden geeft de naam terug.

--- Fitschool/Form1.cs
+++ Fitschool/Form1.cs
@@ -1,3 +1,6 @@
+using MySqlConnector;
+
+
 namespace Fitschool
 {
     public partial class Form1 : Form
@@ -5,12 +8,46 @@ namespace Fitschool
         public Form1()
         {
             InitializeComponent();
-            MessageBox.Show("Coole shit, vrouw");
         }
+
+        string connectionAdress = "Server=myServerAddress;Database=myDatabase;User=myUsername;Password=myPassword;";
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Test");
+            using (MySqlConnection connection = new MySqlConnection(connectionAdress))
+            {
+                try
+                {
+                    connection.Open();
+
+                    string query = "SELECT * FROM YourTableName WHERE ID = @id";
+
+                    using (MySqlCommand command = new MySqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@id", 12); // ID 12 als voorbeeld, dit moet dadelijk van het pasje komen.
+
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                //voorbeeld: naam is de huidige collom die word opgevraagd.
+                                string value = reader["naam"].ToString();
+
+                                MessageBox.Show("Data voor ID 12: " + value);
+                            }
+                            else
+                            {
+                                MessageBox.Show("Geen naam gevonden voor ID 12");
+                            }
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Fout bij verbinden met MySQL database");
+                }
+                
+            }
         }
     }
 }
