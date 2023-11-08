ff0a811d6ba2d2107242195e4cccb161933e29b1
MaxDelissen <maxdelissen17@gmail.com>
2023-11-08 15:30:55 +0100

    Bezig met fixen database connectie
    Werkt helaas nog niet :(

--- Fitschool/Database.cs
+++ Fitschool/Database.cs
@@ -1,8 +1,9 @@
-﻿using MySqlConnector;
+﻿using MySql;
+using MySql.Data.MySqlClient;
 
 public class DataManagement
 {
-    string connectionAdress = "Server=myServerAddress;Database=myDatabase;User=myUsername;Password=myPassword;";
+    string connectionAdress = "server=192.168.154.75;database=test;uid=Max;password=Password01;";
 
     public string IdToName(int id) //functie om naam op te halen.
     {
@@ -28,57 +29,65 @@ public class DataManagement
 
     public string RetrieveFromDB(int id, string columm) //functie om data uit de database te vragen, Neemt een student id, en de colom om de data uit te halen.
     {
-        using (MySqlConnection connection = new MySqlConnection(connectionAdress))
+        MySqlConnection connection = new MySqlConnection();
+        connection.ConnectionString = connectionAdress;
+
+        try
         {
-            try
-            {
-                //verbinding maken met SQL database, wanneer dit niet werkt error vangen, zie onderaan.
-                connection.Open();
+            //verbinding maken met SQL database, wanneer dit niet werkt error vangen, zie onderaan.
+            connection.Open();
 
-                //SQL query, Kan misschien nog beter?
-                string query = "SELECT * FROM YourTableName WHERE ID = @id";
+            //SQL query, Kan misschien nog beter?
+            string query = "SELECT * FROM YourTableName WHERE ID = @id";
 
-                using (MySqlCommand command = new MySqlCommand(query, connection))
+            MySqlCommand command = new MySqlCommand(query, connection);
+            command.Parameters.AddWithValue("@id", id);
+
+                using (MySqlDataReader reader = command.ExecuteReader())
                 {
-                    command.Parameters.AddWithValue("@id", id);
-
-                    using (MySqlDataReader reader = command.ExecuteReader())
+                    if (reader.Read())//controleerd of data is gevonden voor ID;
                     {
-                        if (reader.Read())//controleerd of data is gevonden voor ID;
+                        if (string.IsNullOrEmpty(columm))
                         {
-                            if (string.IsNullOrEmpty(columm))
-                            {
-                                MessageBox.Show("Invalid column name.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                                connection.Close();
-                                return "Error: Invalid column name.";
-                            }
-
-                            string value = reader[columm].ToString();
+                            MessageBox.Show("Invalid column name.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                             connection.Close();
-                            if (value != null)
-                            {
-                                return value;
-                            }
-                            else
-                            {
-                                return "Error: Column was empty.";
-                            }
+                            return "Error: Invalid column name.";
                         }
-                        else //== geen data gevonden voor het opgevraagde id, mogelijk geen id.
+
+                        string value = reader[columm].ToString();
+                        connection.Close();
+                        if (value != null)
                         {
-                            MessageBox.Show("Geen gegevens gevonden voor ID " + id, "Geen gegevens voor " + id, MessageBoxButtons.OK, MessageBoxIcon.Question);
-                            connection.Close();
-                            return ("Error: No data found");
+                            return value;
+                        }
+                        else
+                        {
+                            return "Error: Column was empty.";
                         }
                     }
+                    else //== geen data gevonden voor het opgevraagde id, mogelijk geen id.
+                    {
+                        MessageBox.Show("Geen gegevens gevonden voor ID " + id, "Geen gegevens voor " + id, MessageBoxButtons.OK, MessageBoxIcon.Question);
+                        connection.Close();
+                        return ("Error: No data found");
+                    }
                 }
-            }
-            catch (Exception) //Fout wanneer de opgegeven MySQL database niet kan worden gevonden.
-            {
-                MessageBox.Show("Fout bij verbinden met MySQL database", "Verbindingsfout", MessageBoxButtons.OK, MessageBoxIcon.Hand);
-                return ("Error: Database connection failed");
-            }
-
         }
+        catch (Exception expetion) //Fout wanneer de opgegeven MySQL database niet kan worden gevonden.
+        {
+            MessageBox.Show("Fout bij verbinden met MySQL database", "Verbindingsfout", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            return ("Error: Database connection failed" + expetion);
+        }
+
+        
     }
+
+    public void WritePointsToDB(int id, int pointsToAdd)
+    {
+        int currentPoints = IdToPoints(id);
+        int newPoints = currentPoints + pointsToAdd;
+
+        //todo: finish method
+    }
+
 }
